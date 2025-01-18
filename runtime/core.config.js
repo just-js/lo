daedf84c7e191b19fb59de2d28b077caa8c58c21
Andrew Johnston <apjohnsto@gmail.com>
2025-01-18 21:11:29 +0000

    fix socket bug

--- runtime/core.config.js
+++ runtime/core.config.js
@@ -4,11 +4,11 @@ const bindings = [
 const libs = []
 const embeds = []
 const target = 'lo'
-const opt = '-O3 -march=native -mtune=native'
+const opt = '-O3 -march=native -mtune=native -std=c++20 -c -fno-omit-frame-pointer -fno-rtti -fno-exceptions'
 
 const v8_opts = {
   v8_cleanup: 0, v8_threads: 2, on_exit: 0,
-  v8flags: '--stack-trace-limit=10 --use-strict --turbo-fast-api-calls --no-freeze-flags-after-init --cppgc-young-generation'
+  v8flags: '--stack-trace-limit=10 --use-strict --turbo-fast-api-calls --no-freeze-flags-after-init'
 }
 
 let link_type = '-rdynamic'
