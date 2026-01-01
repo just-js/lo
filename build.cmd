eccf0d326bc6a5b6dea982933439df90e5a98af3
Andrew Johnston <apjohnsto@gmail.com>
2026-01-01 14:49:54 +0000

    chore: build inflate for windows

--- build.cmd
+++ build.cmd
@@ -11,6 +11,11 @@ if exist lo.exe (
 )
 clang-cl.exe /GS- -m64 /EHsc /std:c++20 /I. /I./v8 /I./v8/include /c -DV8_TYPED_ARRAY_MAX_SIZE_IN_HEAP=64 -DV8_ALLOCATION_FOLDING -DV8_SHORT_BUILTIN_CALLS lib/win/win.cc
 clang-cl.exe /GS- -m64 /EHsc /std:c++20 /I. /I./v8 /I./v8/include /c -DV8_TYPED_ARRAY_MAX_SIZE_IN_HEAP=64 -DV8_ALLOCATION_FOLDING -DV8_SHORT_BUILTIN_CALLS -D_CRT_SECURE_NO_WARNINGS lib/core2/core.cc
+cd lib\inflate
+curl -s -O https://raw.githubusercontent.com/emmanuel-marty/em_inflate/master/lib/em_inflate.h
+curl -s -O https://raw.githubusercontent.com/emmanuel-marty/em_inflate/master/lib/em_inflate.c
+clang -I. -c -o em_inflate.o -O3 em_inflate.c
+cd ..\..
 clang-cl.exe /GS- -m64 /EHsc /std:c++20 /I. /I./lib/inflate /I./v8 /I./v8/include /c -DV8_TYPED_ARRAY_MAX_SIZE_IN_HEAP=64 -DV8_ALLOCATION_FOLDING -DV8_SHORT_BUILTIN_CALLS lib/inflate/inflate.cc
 clang-cl.exe /GS- -m64 /EHsc /std:c++20 /I. /I./v8 /I./v8/include /c -DV8_TYPED_ARRAY_MAX_SIZE_IN_HEAP=64 -DV8_ALLOCATION_FOLDING -DV8_SHORT_BUILTIN_CALLS -DVERSION=\"%VERSION%\" -DRUNTIME=\"%RUNTIME%\" lo.cc
 clang-cl.exe /GS- -m64 /EHsc /std:c++20 /I. /I./v8 /I./v8/include /c -DV8_TYPED_ARRAY_MAX_SIZE_IN_HEAP=64 -DV8_ALLOCATION_FOLDING -DV8_SHORT_BUILTIN_CALLS -DVERSION=\"%VERSION%\" -DRUNTIME=\"%RUNTIME%\" main.cc
