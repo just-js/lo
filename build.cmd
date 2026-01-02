95ac83460a28e91db4e6cd3bbf0ae28b2860fef7
Andrew Johnston <apjohnsto@gmail.com>
2026-01-02 01:11:48 +0000

    chore: check in core2 source

--- build.cmd
+++ build.cmd
@@ -32,6 +32,9 @@ clang++ %OPTS% %WARN% %INCLUDE% -c %V8_OPTS% lib/win/win.cc
 clang++ %OPTS% %WARN% %INCLUDE% -c %V8_OPTS% -D_CRT_SECURE_NO_WARNINGS lib/core2/core.cc
 clang++ %OPTS% %WARN% %INCLUDE% -c %V8_OPTS% -DVERSION=\"%VERSION%\" -DRUNTIME=\"%RUNTIME%\" lo.cc
 clang++ %OPTS% %WARN% %INCLUDE% -c %V8_OPTS% -DVERSION=\"%VERSION%\" -DRUNTIME=\"%RUNTIME%\" main.cc
+REM clang++ %OPTS% %INCLUDE% -Iscratch\curl\curl-8.17.0_6-win64-mingw\include -c %V8_OPTS% -DNOMINMAX lib/curl/curl.cc
+REM set CURLP=scratch\curl\curl-8.17.0_6-win64-mingw\
+REM clang++ v8\v8_monolith.lib lo.o main.o win.o core.o inflate.o lib\inflate\em_inflate.o curl.o %CURLP%lib\libcurl.a %CURLP%lib\libcurl.dll.a -lwinmm -ldbghelp -lbcrypt -o lo.tmp.exe
 clang++ v8\v8_monolith.lib %OBJS% %LOPTS% -o %RUNTIME%.tmp.exe
 move /Y %RUNTIME%.tmp.exe %RUNTIME%.exe
 del *.lib
