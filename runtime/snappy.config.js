8e2bff73d2915b66cd5929c283be7a5781bcd7a0
Andrew Johnston <billy@billywhizz.io>
2026-08-27 22:29:36 +0000

    chore: use normal v8 flags for snappy runtime

--- runtime/snappy.config.js
+++ runtime/snappy.config.js
@@ -9,7 +9,7 @@ if (lo.core.os === 'mac') link_type = '-static-libstdc++ -w -framework CoreFound
 const opt = '-O3 -ffunction-sections -fdata-sections -march=native -mtune=native -std=c++20 -c -fno-omit-frame-pointer -fno-rtti -fno-exceptions -fvisibility=hidden'
 const v8_opts = {
   v8_cleanup: 0, v8_threads: 1, on_exit: 0,
-  v8flags: '--lite-mode --jitless --single-threaded --disable-write-barriers --max-heap-size=16 --no-verify-heap --memory-reducer --optimize-for-size --stack-trace-limit=10 --use-strict --turbo-fast-api-calls',
+  v8flags: '--stack-trace-limit=10 --use-strict --turbo-fast-api-calls --no-freeze-flags-after-init',
   snapshot: true
 }
 const main = 'runtime/snappy.js'
